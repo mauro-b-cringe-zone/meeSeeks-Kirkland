16a858f25297611ee432519a2765ef272e1a3443
maubg-debug <maubg2007@gmail.com>
2020-11-01 11:47:45 +0100

    ...

--- src/consola/main.py
+++ src/consola/main.py
@@ -15,11 +15,11 @@ class CrearEnv():
         if self.checkearPlataforma == "win":
             os.system("py -m pip install --user virtualenv")
             os.system("py -m venv env") 
-            os.system(".\env\Scripts\activate")
+            cprint("Ahora pon en la consola verdadera \".\env\Scripts\activate\" ara activarlo")
         elif self.checkearPlataforma == "lin":
             os.system("python3 -m pip install --user virtualenv")
             os.system("python3 -m venv env ")
-            os.system("source env/bin/activate")
+            cprint("Ahora pon en la consola verdadera \"source env/bin/activate\" ara activarlo")
         cprint("\nPuedes poner 'deactivate' para quitar el virtual env\n", "green")
 
 class Consola():
