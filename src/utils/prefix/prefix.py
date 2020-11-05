cd41f9520847e4fbc7b753fadeb4dc99d25423b6
maubg-debug <maubg2007@gmail.com>
2020-11-05 16:46:06 +0100

    Mejores prefijos

--- src/utils/prefix/prefix.py
+++ src/utils/prefix/prefix.py
@@ -1,10 +1,18 @@
 import json
 
 def get_prefix(bot, message):
-    try:
+    """
+    Tendremos que coger los prefijos cadavez que esta funcion se llame
+    necesitamos pasar el mensage para ver el mensage del servidor
+    Si el mensage es de un servidor:
+    :return: El array con los prefijos ['?', '!', 'm.'] y con el prefijo personalizado
+    Si es un mensage directo
+    :return: @#~€ (Tiene que ser algo dificil)
+    """
+    if message.guild is not None:
         with open('./src/json/prefix.json', 'r') as f:
             prefixes = json.load(f)
         base = [prefixes[str(message.guild.id)], '?', '!', 'm.']
         return base
-    except:
-        return "m.-"
+    else:
+        return "@#~€" # Retornar algo que sea imposible de activar como prefijo
