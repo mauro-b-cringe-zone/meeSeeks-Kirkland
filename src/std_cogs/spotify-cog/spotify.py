a5da0779fa80954c8a49a6d0dff483ef7a05322d
maubg-debug <maubg2007@gmail.com>
2020-11-10 19:36:12 +0100

    Update spotify.py

--- src/std_cogs/spotify-cog/spotify.py
+++ src/std_cogs/spotify-cog/spotify.py
@@ -150,7 +150,7 @@ class Spotify(commands.Cog):
             DURATION_LEFT_SIZE = DRAW.textsize(STR_END, font=SUBTITLE_FONT)[0]
 
             DRAW.rectangle([(MARGIN_LEFT, MARGIN_TOP + 70), (MARGIN_RIGHT, MARGIN_TOP + 120)], fill=tuple(map(lambda x: x - 25, BACKGROUND_COLOR)))
-            DRAW.rectangle([(MARGIN_LEFT, MARGIN_TOP + 70), (80) + MARGIN_LEFT, MARGIN_TOP + 120)], fill="#2ecc71")
+            DRAW.rectangle([(MARGIN_LEFT, MARGIN_TOP + 70), (80)], fill="#2ecc71")
             DRAW.text((MARGIN_LEFT, MARGIN_TOP + 130), STR_CURRENT, font=SUBTITLE_FONT, fill="#2ecc71")
             DRAW.text((MARGIN_RIGHT - DURATION_LEFT_SIZE, MARGIN_TOP + 130), STR_END, font=SUBTITLE_FONT, fill="#2ecc71")
 
