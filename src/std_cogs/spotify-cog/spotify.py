bc4055b5eb6e20fa06a83cc66d2335740f34a539
maubg-debug <maubg2007@gmail.com>
2020-11-10 19:18:36 +0100

    Update spotify.py

--- src/std_cogs/spotify-cog/spotify.py
+++ src/std_cogs/spotify-cog/spotify.py
@@ -158,7 +158,7 @@ class Spotify(commands.Cog):
         DRAW.text((MARGIN_LEFT, MARGIN_TOP + 38), SUBTITLE_TEXT, font=SUBTITLE_FONT, fill=FOREGROUND_COLOR)
         DRAW.text((MARGIN_LEFT, MARGIN_TOP + 65), DESC_TEXT, font=DESC_FONT, fill=FOREGROUND_COLOR)
 
-        MAIN.paste(COVER, (20, 20))
+        MAIN.paste(COVER, (25, 10))
         
         return self.buffer(MAIN)
 
