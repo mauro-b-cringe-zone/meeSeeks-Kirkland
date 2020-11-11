c6a9cd7c9780763be1a54acbb29200b74e1ece31
Maubg <68229077+maubg-debug@users.noreply.github.com>
2020-11-11 23:07:27 +0100

    Update prefix.py

--- src/utils/prefix/prefix.py
+++ src/utils/prefix/prefix.py
@@ -1,6 +1,5 @@
 import json
 from os import environ as env
-
 from discord.ext import commands
 
 def get_prefix(bot, message):
@@ -17,11 +16,21 @@ def get_prefix(bot, message):
     """
     if message.guild is not None:
         try:
+            msg = str(message.content)
+            id = str(f'<@!{bot.user.id}>')
+            if id in msg or msg in f"<@!{bot.user.id}> prefijos":
+                return ["$", '?', '!', 'm.']
             with open(env["JSON_DIR"] + 'prefix.json', 'r') as f:
                 prefixes = json.load(f)
+            if not str(message.guild.id) in prefixes:
+                prefixes[str(message.guild.id)] = "$"
+                with open(env["JSON_DIR"] + 'prefix.json', 'w') as f:
+                    json.dump(prefixes, f) 
+                return [prefixes[str(message.guild.id)], '?', '!', 'm.']
             base = [prefixes[str(message.guild.id)], '?', '!', 'm.']
-            return commands.when_mentioned_or(*base)(bot, message)
-        except:
+            return commands.when_mentioned_or(base)(bot, message)
+        except Exception as e:
+            print(e)
             return ['$', '?', '!', 'm.']
     else:
         return "$" # Si no esta en un servidor retornar el prefjio "$"
