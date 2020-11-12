a5acb505846f6f538e33f7fcd2e05ff9571d05e4
maubg-debug <maubg2007@gmail.com>
2020-11-12 18:47:12 +0100

    Update App.py

--- src/App.py
+++ src/App.py
@@ -74,7 +74,7 @@ class App(commands.Bot):
         # await self.change_presence(activity=self.__activity)
         while True:
             # await self.barra_de_actividad(f"|  $help  |  {len(self.users)} Usuarios en  {len(self.guilds)} servidores | con 186 commandos", discord.Status.do_not_disturb)
-            await self.barra_de_actividad(f"|-. Processando $help y @mencion -|", discord.Status.dnd)
+            await self.barra_de_actividad(f"|-> Processando $help y @mencion -|", discord.Status.dnd)
 
     async def on_message(self, message):
         await run_middleware_stack(message)
