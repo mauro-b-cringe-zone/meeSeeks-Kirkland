8f46ac70a9510f1ddf61735377c2a7ed99b41005
maubg-debug <maubg2007@gmail.com>
2020-11-12 17:55:50 +0100

    Update spotify.py

--- src/std_cogs/spotify-cog/spotify.py
+++ src/std_cogs/spotify-cog/spotify.py
@@ -161,6 +161,7 @@ class Spotify(commands.Cog):
         DRAW.text((MARGIN_LEFT, MARGIN_TOP + 40), DESC_TEXT, font=DESC_FONT, fill=FOREGROUND_COLOR)
 
         MAIN.paste(COVER, (25, 10))
+        SPOTIFY_ICON.rotate(45)
         MAIN.paste(SPOTIFY_ICON, (400, 10), SPOTIFY_ICON)
         
         return self.buffer(MAIN)
