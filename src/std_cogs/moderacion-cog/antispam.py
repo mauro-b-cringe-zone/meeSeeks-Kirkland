8a5cced4dc67ee05e120e0e68f692831eb92a008
maubg-debug <maubg2007@gmail.com>
2020-11-14 17:11:52 +0100

    Update antispam.py

--- src/std_cogs/moderacion-cog/antispam.py
+++ src/std_cogs/moderacion-cog/antispam.py
@@ -22,10 +22,14 @@ class AntiSpam(commands.Cog):
 
         if not str(ctx.guild.id) in guild:
             guild[str(ctx.guild.id)] = True
+            await ctx.send(embed=discord.Embed(title=f"Se ha cambiado el estado a | verdadero", description=f"**¿Qué implica esto?**\n\n<:list:774983585727119391> No mas de **10** menciones\n<:list:774983585727119391> No mas de **2000** caracteres en un mensage", color=color))
         else:
-            if guild[str(ctx.guild.id)] is True: guild[str(ctx.guild.id)] = False
-            elif guild[str(ctx.guild.id)] is False: guild[str(ctx.guild.id)] = True
-        await ctx.send(embed=discord.Embed(title=f"Se ha cambiado el estado a | {guild[str(ctx.guild.id)]}", color=color))
+            if guild[str(ctx.guild.id)] is True: 
+                guild[str(ctx.guild.id)] = False
+                await ctx.send(embed=discord.Embed(title=f"Se ha cambiado el estado a | falso", description=f"**¿Qué implica esto?**\n\Maubot ya no se encargara en analizar los mensages del servidor | **{ctx.guild.name}**", color=color))
+            elif guild[str(ctx.guild.id)] is False: 
+                guild[str(ctx.guild.id)] = True
+                await ctx.send(embed=discord.Embed(title=f"Se ha cambiado el estado a | verdadero", description=f"**¿Qué implica esto?**\n\n<:list:774983585727119391> No mas de **10** menciones\n<:list:774983585727119391> No mas de **2000** caracteres en un mensage", color=color))
 
         with open(str(env["JSON_DIR"] + "ext/seguridad.json"), "w") as f:
             json.dump(guild, f, indent=4)
@@ -37,6 +41,9 @@ class AntiSpam(commands.Cog):
         if d:
             if len(msg.raw_mentions) >= 10:
                 await msg.channel.send(embed=discord.Embed(title=f"Demasiado...", description=f"{ctx.author.mention} Este servidor esta en modo antiespam asique no puedes poner **mas de 10** menciones", color=color).set_footer(text="$seguridad | Para desactivarlo"))
+                await msg.delete()
+            if len(msg.content) >= 2000:
+                await msg.channel.send(embed=discord.Embed(title=f"Demasiado...", description=f"{ctx.author.mention} Este servidor esta en modo antiespam asique no puedes poner **mas de 2000** caracteres", color=color).set_footer(text="$seguridad | Para desactivarlo"))
                 await msg.delete()
         else:
             pass
