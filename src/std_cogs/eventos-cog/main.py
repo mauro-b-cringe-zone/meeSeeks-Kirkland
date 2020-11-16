ad9ac63f5d4e6c9de0058514deb40cebd401b897
maubg-debug <maubg2007@gmail.com>
2020-11-16 17:17:48 +0100

    Musica

--- src/std_cogs/eventos-cog/main.py
+++ src/std_cogs/eventos-cog/main.py
@@ -42,6 +42,8 @@ class Servidor(commands.Cog):
                     # print(dest)
                     if str(dest) in chats["chats"]:
                         destid, dest = int(dest), self.bot.get_user(int(dest))
+                        if "http" in message.content:
+                            await message.author.send(embed=discord.Embed(title="NADA DE LINKS", description=f"{message.author.mention} NO SE ACEPTAN LINKS", color=color))
                         if message.content == "cerrarchat":
                             await message.author.send(embed=discord.Embed(title="El chat esta cerrado", description=f"{message.author.mention} se ha cerrado la conexion con **{dest.mention}**", color=color))
                             await dest.send(embed=discord.Embed(title="El chat esta cerrado", description=f"{dest.mention}, **{message.author.mention}** Ha cerrado la conexion con el chat.", color=color))
