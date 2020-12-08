67cfaf4d12706da7e77aa5f20d1332fc6a41434b
maubg-debug <maubg2007@gmail.com>
2020-12-08 18:08:44 +0100

    Update general.py

--- src/std_cogs/general-cog/general.py
+++ src/std_cogs/general-cog/general.py
@@ -74,6 +74,12 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(description="Un url-shortener en maubot")
+    @commands.cooldown(1, 5, commands.BucketType.user)       
+    async def url(self, ctx, url: str):
+        codigo = requests.get(f"https://maubot.maucode.com/redir/crear?url={url}").json()
+        await ctx.send(embed=discord.Embed(color=color, title=f"Se te ha creado una url | {codigo['codigo']}", description=f"Puedes visitarlo dandole a este link ([https://maubot.maucode.com/redir?codigo={codigo['codigo']}](https://maubot.maucode.com/redir?codigo={codigo['codigo']}))"))
+
     @commands.command(description="Mira la tardanza de respuesta")
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def ping(self, ctx):
