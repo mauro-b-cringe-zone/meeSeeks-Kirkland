921e0c304ffe51c37fba8b94eac29a73512f484e
maubg-debug <maubg2007@gmail.com>
2020-12-09 16:36:31 +0100

    ups...

--- src/std_cogs/general-cog/general.py
+++ src/std_cogs/general-cog/general.py
@@ -890,6 +890,34 @@ class GeneralSecundario(commands.Cog):
                 embed = discord.Embed(title=pageTitle, url=str(page.fullurl), description=str(explain), colour=0xf7f7f7)
                 await wait.edit(content='', embed=embed)
 
+# import asyncio, time
+
+
+
+# class Reminder(commands.Cog):
+#     def __init__(self, bot):
+#         self.bot = bot     
+#         self.color = int(env["COLOR"])
+
+#     def CalcularTiempo(self, t: float):
+#         return t * 60
+
+#     @commands.command(aliases=["remind"], description="Se te recordara una tarea que tu quieras", usage="<Tiempo: minutos> <Recordatorio: texto>")
+#     @commands.cooldown(1, 20, commands.BucketType.user)
+#     async def reminder(self, ctx: commands.Context, tiempo: float, *, texto):
+#         autor = ctx.author
+#         if tiempo is None and texto is None:
+#             return await ctx.send(embed=discord.Embed(color=self.color, title="No has puesto el tiempo bien", description=f"{ctx.author.mention} no has puesto el tiempo correctamente\n**eg: m.reminder [minutos] <texto>**"))
+#         t = self.CalcularTiempo(tiempo)
+#         await ctx.send(embed=discord.Embed(color=self.color, title="Recordatorio añadido", description=f"{autor.mention} se te recordara en dentro de **{tiempo}** minuto/s"))
+#         await asyncio.sleep(t)
+#         start = time.perf_counter()
+#         recormsg = await autor.send(embed=discord.Embed(color=self.color, title="RECORDATORIO", description=f"{ctx.author.mention} Has iniciado un recordatorio hace **{tiempo}** minutos. ").add_field(name="Recordatorio", value=texto))
+#         end = time.perf_counter()
+#         speed = round((end - start) * 1000)
+#         await recormsg.edit(embed=discord.Embed(color=self.color, title="RECORDATORIO", description=f"{ctx.author.mention} Has iniciado un recordatorio hace **{tiempo}** minutos. ").add_field(name="Recordatorio", value=texto).add_field(name="tardanza", description=speed + "ms", inline=False))
+
 def setup(bot):
+    # bot.add_cog(Reminder(bot))
     bot.add_cog(General(bot))
     bot.add_cog(GeneralSecundario(bot))
