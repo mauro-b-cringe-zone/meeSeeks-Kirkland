aecd8021829e61fe8a6b6fdea3fd170e4af29ceb
Maubg <68229077+maubg-debug@users.noreply.github.com>
2020-12-10 21:12:41 +0100

    Update general.py

--- src/std_cogs/general-cog/general.py
+++ src/std_cogs/general-cog/general.py
@@ -118,37 +118,14 @@ class General(commands.Cog):
         embed.set_footer(text=f"Tiempo total estimado transcurrido: {round(sum(times))}ms")
         await msg.edit(content=f":ping_pong: **{round((round(sum(times)) + round(self.bot.latency * 1000))/4)}ms**", embed=embed)
 
-    # @commands.command()
-    # async def giveaway(self, ctx, mins: int, *, description: str):
-    #     embed = discord.Embed(title="Regalo", description=description, colour=color)
-    #     embed.add_field(name="Tiempo limite", value=f"{datetime.utcnow()+timedelta(seconds=mins*60)} UTC", inline=False)
-    #     message = await ctx.send(embed=embed)
-    #     await message.add_reaction("✅")
-
-    #     self.giveaways.append((message.channel.id, message.id))
-
-    #     self.bot.scheduler.add_job(self.complete_giveaway, "date", run_date=datetime.now()+timedelta(seconds=mins),
-    #                                 args=[message.channel.id, message.id])
-
-    # async def complete_poll(self, channel_id, message_id):
-    #     message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-
-    #     most_voted = max(message.reactions, key=lambda r: r.count)
-
-    #     await message.channel.send(f"The results are in and option {most_voted.emoji} was the most popular with {most_voted.count-1:,} votes!")
-    #     self.polls.remove((message.channel.id, message.id))
-
-    # async def complete_giveaway(self, channel_id, message_id):
-    #     message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-
-    #     if len((entrants := [u for u in await message.reactions[0].users().flatten() if not u.bot])) > 0:
-    #         winner = choice(entrants)
-    #         await message.channel.send(f"Congratulations {winner.mention} - you won the giveaway!")
-    #         self.giveaways.remove((message.channel.id, message.id))
-
-    #     else:
-    #         await message.channel.send("Giveaway ended - no one entered!")
-    #         self.giveaways.remove((message.channel.id, message.id))
+    @commands.command(aliases='useless,uselesssites,uselessweb,uselesswebsites,uselesswebsite'.split(","))
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    async def uselesswebs(self, ctx):
+        try:
+            url = requests.get('https://useless-api.vierofernando.repl.co/uselesssites').json()["url"]
+            await ctx.send(embed=discord.Embed(title="Web inutil", description=f'**{url}**', color=color))
+        except:
+            await ctx.send(f'Ups. hay algún error, mientras tanto mira este sitio inútil: <https://top.gg/bot/730124969132163093/vote>')
 
     @staticmethod
     def get_channel_type_counts(guild: Guild):
