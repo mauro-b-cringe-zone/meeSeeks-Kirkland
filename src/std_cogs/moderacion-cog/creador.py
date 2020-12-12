aedba9ca405b0803ab852503f25171ba7db6326d
maubg-debug <maubg2007@gmail.com>
2020-12-12 17:46:18 +0100

    Update creador.py

--- src/std_cogs/moderacion-cog/creador.py
+++ src/std_cogs/moderacion-cog/creador.py
@@ -90,7 +90,7 @@ class Creador(commands.Cog):
 
 
     @commands.command(aliases=['ex','eval'], description="Evalua lo que quieras", usage="<texto>")
-    async def evaluate(self, ctx, *args):
+    async def evaluate(self, ctx, *, args):
         unprefixed = ' '.join(list(args.replace('"', "'")))
         if int(ctx.message.author.id)==700812754855919667:
             try:
