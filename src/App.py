527087bd06906ce7ac991608c5a03e48986b7a19
maubg-debug <maubg2007@gmail.com>
2020-12-19 14:41:33 +0100

    Update App.py

--- src/App.py
+++ src/App.py
@@ -52,7 +52,7 @@ class App(commands.Bot):
         self.add_command(App.__reload_cogs)
         
     async def on_ready(self):
-        await self.change_presence(activity=discord.Game(name=self.__estado), status=discord.Status.do_not_disturb)
+        await self.change_presence(activity=discord.Game(name=self.__estado), status=discord.Status.do_not_disturb, afk=True)
         Logger.success(f"--------------------------------------------------------------------------------------------------\nInfo: \n1. Autor              | {self.__autor__}\n2. Github del creador | {self.__github__}\n3. Repo de maubot     | {self.__repo__}\n4. Version            | {self.__version__}\n5. Web                | {self.__web__}", separador=False)
         Logger.success(f'Maubot esta online como "{self.user}".', separador=True)
 
