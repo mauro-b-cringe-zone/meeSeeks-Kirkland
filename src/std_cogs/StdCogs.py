df955a5d5503581a0fafb6113e07897a951a8d9e
Maubg <68229077+maubg-debug@users.noreply.github.com>
2020-12-28 22:31:11 +0100

    Update StdCogs.py

--- src/std_cogs/StdCogs.py
+++ src/std_cogs/StdCogs.py
@@ -3,11 +3,18 @@ import os
 from cogs.Cogs import Cogs
 from utils.Environment import env
 
-
 class StdCogs(Cogs):
     __STD_COG_PATH = 'std_cogs'
     __cogs: list
     __cog_path: str
+
+    def __mas_cmds(self, archivo, nombre_archivo):
+        if nombre_archivo == "http_cmds":
+            for i in os.listdir(f"{self.__cog_path}/{archivo}/{nombre_archivo}"):
+                if i.startswith("cmd_"):
+                    self.__cogs.append(f"std_cogs.fun-cog.http_cmds.{i[:-3]}")
+                return True
+        return False
 
     def __init__(self):
         self.__cog_path = f'{env.get("BOT_ROOT_PATH")}/{self.__STD_COG_PATH}'
@@ -16,27 +23,11 @@ class StdCogs(Cogs):
             if archivo != "__pycache__":
                 if self.__es_cog(archivo):
                     for nombre_archivo in os.listdir(f"{self.__cog_path}/{archivo}"):
-                        if nombre_archivo == "http_cmds":
-                            for i in os.listdir(f"{self.__cog_path}/{archivo}/{nombre_archivo}"):
-                                if i.startswith("cmd_"):
-                                    self.__cogs.append(f"std_cogs.fun-cog.http_cmds.{i[:-3]}")
-                        if nombre_archivo == "social_cmds":
-                            for i in os.listdir(f"{self.__cog_path}/{archivo}/{nombre_archivo}"):
-                                if not i.startswith("__py"):
-                                    self.__cogs.append(f"std_cogs.fun-cog.social_cmds.{i[:-3]}")
-                        if not nombre_archivo.startswith("__py"):
-                            if not nombre_archivo.endswith(".txt"):
-                                if not nombre_archivo.endswith("_cmds"):
-                                    if not nombre_archivo == "listexe": 
-                                        self.__cogs.append(f'std_cogs.{archivo}.{nombre_archivo[:-3]}')
-                                    else:
-                                        continue
-                                else:
-                                    continue
-                            else:
-                                continue
-                        else:
-                            continue
+                        _mas__cmd = self.__mas_cmds(archivo, nombre_archivo)
+                        if _mas__cmd: continue
+                        if nombre_archivo.endswith(".py"):
+                            self.__cogs.append(f'std_cogs.{archivo}.{nombre_archivo[:-3]}')
+
         self.__cogs.append("utils.help.main")
         self.__cogs.append("App")
 
